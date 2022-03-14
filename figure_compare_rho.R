7a10c91ea58feb2238b8cb37ad7d5a833b59f53f
parksw3 <parksw3@mcmaster.ca>
2022-03-14 15:42:30 -0400

    commit

---
+++ figure_compare_rho.R
@@ -0,0 +1,22 @@
+library(bbmle)
+library(dplyr)
+library(readxl)
+library(ggplot2); theme_set(theme_bw(base_family = "Times"))
+library(egg)
+library(gridExtra)
+load("rdaout/fit_lognormal_base_rho.rda")
+
+g1 <- ggplot(filter(fit_lognormal_base_rho_50_nsgtf_within, param=="mean"))  +
+  geom_line(aes(rho, est), lwd=1, col="black") +
+  geom_line(aes(rho, lwr), lwd=1, col="black") +
+  geom_line(aes(rho, upr), lwd=1, col="black") +
+  geom_line(data=filter(fit_lognormal_base_rho_50_sgtf_within, param=="mean", type=="Omicron"), aes(rho, est), lty=2, col="orange", lwd=1) +
+  geom_line(data=filter(fit_lognormal_base_rho_50_sgtf_within, param=="mean", type=="Omicron"), aes(rho, lwr), lty=2, col="orange", lwd=1) +
+  geom_line(data=filter(fit_lognormal_base_rho_50_sgtf_within, param=="mean", type=="Omicron"), aes(rho, upr), lty=2, col="orange", lwd=1) +
+  scale_x_continuous("Lognormal correlation coefficient") +
+  scale_y_continuous("Mean generation interval (days)") +
+  theme(
+    panel.grid = element_blank()
+  )
+
+ggsave("figure_compare_rho.pdf", g1, width=12, heigh=8)
